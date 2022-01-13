ec8a7887a35fb86f23758f37f1b304c0bff1748f
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-01-13 21:45:40 +0100

    feat: Test responsive navigation

---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      white: "#ffffff",
+      lightgrey: "#c4c4c4",
+      darkergrey: "#403E3E",
+      textgrey: "#332E2E",
+      deepgrey: "#263238",
+      bluishgrey: "#1D3547",
+      fuscia: "#CF375D",
+      gold: "#ECB32C",
+    },
+  },
+});
+export default theme;
