5a034a05952fcbfad2fd5d50cd17d76d822e0a84
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2022-01-25 15:42:31 +0100

    feat: Complete about page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -13,6 +13,16 @@ import {
   Stack,
   Text,
   VStack,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
 } from "@chakra-ui/react";
 
 import { AiFillFacebook, AiFillLinkedin } from "react-icons/ai";
@@ -25,6 +35,7 @@ import MemberA from "../assets/memberA.png";
 import MemberB from "../assets/memberB.png";
 import MemberC from "../assets/memberC.png";
 import { Hourglass, Tuition } from "../assets/icons";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function AboutUs() {
   const team = [
@@ -300,68 +311,369 @@ export default function AboutUs() {
             ))}
           </Flex>
         </Stack>
-        <Flex
-          w={"100%"}
-          bg={"white"}
-          justifyContent={{ base: "space-evenly", lg: "flex-start" }}
-        >
-          <Stack
-            w={{ base: "350px", lg: "530px" }}
-            h={"auto"}
-            alignItems={"flex-end"}
-            bg={"brand.deepgrey"}
-            borderTopLeftRadius={{ base: "15px", lg: "0" }}
-            borderBottomLeftRadius={{ base: "15px", lg: "0" }}
-            borderTopRightRadius={"8px"}
-            borderBottomRightRadius={"8px"}
-            py={{ base: "60px", lg: "40px" }}
-            spacing={[4, null, 8]}
+        <Stack w={"100%"} alignItems={{ base: "center", lg: "flex-start" }}>
+          <Flex
+            w={"85%"}
+            direction={{ base: "column", lg: "row" }}
+            justifyContent={{ base: "space-around", lg: "space-between" }}
+            bg={"white"}
+            mt={"100px"}
           >
-            <Flex
-              width={"350px"}
-              alignItems={"center"}
+            <Stack
+              w={{ base: "350px", lg: "530px" }}
+              h={{ base: "auto", lg: "541px" }}
+              alignItems={"flex-end"}
               justifyContent={"space-around"}
+              bg={"brand.deepgrey"}
+              borderTopLeftRadius={{ base: "15px", lg: "0" }}
+              borderBottomLeftRadius={{ base: "15px", lg: "0" }}
+              borderTopRightRadius={"8px"}
+              borderBottomRightRadius={"8px"}
+              py={{ base: "60px", lg: "40px" }}
+              spacing={4}
             >
               <Flex
-                py={8}
-                width={{ base: "90px", lg: "114px" }}
-                h={{ base: "90px", lg: "114px" }}
-                justifyContent={"center"}
+                width={"350px"}
                 alignItems={"center"}
-                borderRadius={"50%"}
-                bg={"white"}
+                justifyContent={"space-around"}
               >
-                <Icon as={Tuition} width={"57px"} h={"57px"} />
+                <Flex
+                  py={8}
+                  width={{ base: "90px", lg: "114px" }}
+                  h={{ base: "90px", lg: "114px" }}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  borderRadius={"50%"}
+                  bg={"white"}
+                >
+                  <Icon as={Tuition} width={"57px"} h={"57px"} />
+                </Flex>
+                <Stack color={"white"} w={{ base: "40%", lg: "60%" }}>
+                  <Heading>250,000</Heading>
+                  <Text>Tuition Fee</Text>
+                </Stack>
               </Flex>
-              <Stack color={"white"} w={{ base: "40%", lg: "60%" }}>
-                <Heading>250,000</Heading>
-                <Text>Tuition Fee</Text>
-              </Stack>
-            </Flex>
-            <Flex
-              width={"350px"}
-              alignItems={"center"}
-              justifyContent={"space-around"}
-            >
               <Flex
-                py={8}
-                width={{ base: "90px", lg: "114px" }}
-                h={{ base: "90px", lg: "114px" }}
-                justifyContent={"center"}
+                width={"350px"}
                 alignItems={"center"}
-                borderRadius={"50%"}
-                bg={"white"}
+                justifyContent={"space-around"}
               >
-                <Icon as={Hourglass} width={"57px"} h={"57px"} />
+                <Flex
+                  py={8}
+                  width={{ base: "90px", lg: "114px" }}
+                  h={{ base: "90px", lg: "114px" }}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  borderRadius={"50%"}
+                  bg={"white"}
+                >
+                  <Icon as={Hourglass} width={"57px"} h={"57px"} />
+                </Flex>
+                <Stack color={"white"} w={{ base: "40%", lg: "60%" }}>
+                  <Heading>265</Heading>
+                  <Text>Days</Text>
+                </Stack>
               </Flex>
-              <Stack color={"white"} w={{ base: "40%", lg: "60%" }}>
-                <Heading>265</Heading>
-                <Text>Days</Text>
-              </Stack>
-            </Flex>
+            </Stack>
+            <Stack w={{ base: "100%", lg: "541px" }}>
+              <Heading py={"15px"}>Financing Options</Heading>
+              <Tabs variant="unstyled">
+                <TabList>
+                  <Tab
+                    color="brand.deepgrey"
+                    bg="brand.lightpink"
+                    _focus={{ boxShadow: "none" }}
+                    _selected={{
+                      color: "brand.lightpink",
+                      bg: "brand.deepgrey",
+                    }}
+                    borderWidth={"1px"}
+                    borderStyle={"solid"}
+                    borderColor={"brand.deepgrey"}
+                    borderTopLeftRadius={4}
+                    borderBottomLeftRadius={4}
+                    py="10px"
+                    px="15px"
+                  >
+                    Self Payment Plan
+                  </Tab>
+                  <Tab
+                    color="brand.deepgrey"
+                    bg="brand.lightpink"
+                    _focus={{ boxShadow: "none" }}
+                    _selected={{
+                      color: "brand.lightpink",
+                      bg: "brand.deepgrey",
+                    }}
+                    borderWidth={"1px"}
+                    borderStyle={"solid"}
+                    borderColor={"brand.deepgrey"}
+                    borderTopRightRadius={4}
+                    py="10px"
+                    px="15px"
+                  >
+                    Financial support
+                  </Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel px={0} py={8}>
+                    <Text mb={"40px"}>
+                      Unlike other courses who tuck away their prices, we are
+                      totally transparent with our online Bootcamp pricing and
+                      take pride in being accessible and affordable.Unlike other
+                      courses who tuck away their prices, we are totally
+                      transparent with our online Bootcamp pricing and take
+                      pride in being accessible and affordable.
+                    </Text>
+                    <ReactRouterLink to="/application">
+                      <Flex
+                        w={"100%"}
+                        alignItems={"center"}
+                        fontSize={"18px"}
+                        color={"brand.fuscia"}
+                        fontWeight={"bold"}
+                      >
+                        <Text mr={2}>Enroll now</Text>{" "}
+                        <BsArrowRight size={"20px"} />
+                      </Flex>
+                    </ReactRouterLink>
+                  </TabPanel>
+                  <TabPanel px={0} py={8}>
+                    <Text mb={"40px"}>
+                      Backend web developers are responsible for designing,
+                      building, and maintaining a scalable “behind-the-scenes”
+                      infrastructure to support the user interfaces and user
+                      experiences built by frontend developers.This skill set is
+                      one of the most sought-after by major tech employers.
+                    </Text>
+                    <ReactRouterLink to="/application">
+                      <Flex
+                        w={"100%"}
+                        alignItems={"center"}
+                        fontSize={"18px"}
+                        color={"brand.fuscia"}
+                        fontWeight={"bold"}
+                      >
+                        <Text mr={2}>Enroll now</Text>{" "}
+                        <BsArrowRight size={"20px"} />
+                      </Flex>
+                    </ReactRouterLink>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Stack>
+          </Flex>
+        </Stack>
+        <Stack w={"100%"} alignItems={"center"} justifyContent={"center"}>
+          <Heading py={"15px"}>FAQs</Heading>
+          <Tabs variant="unstyled" w={{ base: "80%", lg: "70%" }}>
+            <TabList justifyContent={"center"}>
+              <Tab
+                color="brand.deepgrey"
+                bg="brand.lightpink"
+                _focus={{ boxShadow: "none" }}
+                _selected={{
+                  color: "brand.lightpink",
+                  bg: "brand.deepgrey",
+                }}
+                borderWidth={"1px"}
+                borderStyle={"solid"}
+                borderColor={"brand.deepgrey"}
+                borderTopLeftRadius={4}
+                borderBottomLeftRadius={4}
+                py={{ base: "5px", lg: "10px" }}
+                px={{ base: "5px", lg: "15px" }}
+              >
+                About the process
+              </Tab>
+              <Tab
+                color="brand.deepgrey"
+                bg="brand.lightpink"
+                _focus={{ boxShadow: "none" }}
+                _selected={{
+                  color: "brand.lightpink",
+                  bg: "brand.deepgrey",
+                }}
+                borderWidth={"1px"}
+                borderStyle={"solid"}
+                borderColor={"brand.deepgrey"}
+                borderTopRightRadius={4}
+              >
+                About the program
+              </Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel px={0} py={8}>
+                <Accordion allowToggle>
+                  <AccordionItem
+                    borderRightWidth={1}
+                    borderLeftWidth={1}
+                    borderBottomWidth={1}
+                    borderRadius={8}
+                    py={{ base: "10px", lg: "15px" }}
+                    px={{ base: "15px", lg: "30px" }}
+                    marginBottom={"10px"}
+                  >
+                    <h2>
+                      <AccordionButton
+                        _focus={{ boxShadow: "none" }}
+                        _expanded={{ color: "brand.fuscia" }}
+                      >
+                        <Box flex="1" textAlign="left">
+                          Section 1 title
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </AccordionPanel>
+                  </AccordionItem>
+
+                  <AccordionItem
+                    borderRightWidth={1}
+                    borderLeftWidth={1}
+                    borderBottomWidth={1}
+                    borderRadius={8}
+                    py={{ base: "10px", lg: "15px" }}
+                    px={{ base: "15px", lg: "30px" }}
+                    marginBottom={"10px"}
+                  >
+                    <h2>
+                      <AccordionButton
+                        _focus={{ boxShadow: "none" }}
+                        _expanded={{ color: "brand.fuscia" }}
+                      >
+                        <Box flex="1" textAlign="left">
+                          Section 2 title
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </TabPanel>
+              <TabPanel px={0} py={8}>
+                <Accordion allowToggle>
+                  <AccordionItem
+                    borderRightWidth={1}
+                    borderLeftWidth={1}
+                    borderBottomWidth={1}
+                    borderRadius={8}
+                    py={{ base: "10px", lg: "15px" }}
+                    px={{ base: "15px", lg: "30px" }}
+                    marginBottom={"10px"}
+                  >
+                    <h2>
+                      <AccordionButton
+                        _focus={{ boxShadow: "none" }}
+                        _expanded={{ color: "brand.fuscia" }}
+                      >
+                        <Box flex="1" textAlign="left">
+                          Section 3 title
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </AccordionPanel>
+                  </AccordionItem>
+
+                  <AccordionItem
+                    borderRightWidth={1}
+                    borderLeftWidth={1}
+                    borderBottomWidth={1}
+                    borderRadius={8}
+                    py={{ base: "10px", lg: "15px" }}
+                    px={{ base: "15px", lg: "30px" }}
+                    marginBottom={"10px"}
+                  >
+                    <h2>
+                      <AccordionButton
+                        _focus={{ boxShadow: "none" }}
+                        _expanded={{ color: "brand.fuscia" }}
+                      >
+                        <Box flex="1" textAlign="left">
+                          Section 4 title
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Stack>
+        <Stack w={"100%"} py={"100px"} alignItems={"center"}>
+          <Stack
+            pos={"relative"}
+            w={{ base: "90%", lg: "80%" }}
+            h={{ base: "470px", lg: "370px" }}
+            justifyContent={{ base: "space-around", lg: "center" }}
+            alignItems={"center"}
+            color={"white"}
+            textAlign={"center"}
+            bg={"brand.deepgrey"}
+            borderRadius={{ base: "15px", lg: "4px" }}
+            spacing={{ base: 4, lg: 8 }}
+            overflow={"hidden"}
+          >
+            <Box
+              pos={"absolute"}
+              top={0}
+              left={0}
+              display={{ base: "none", lg: "block" }}
+              bg={"brand.gold"}
+              boxSize={"100px"}
+              style={{ clipPath: "polygon(0 0, 100% 0%,  0 100%)" }}
+            ></Box>
+            <Box
+              pos={"absolute"}
+              bottom={0}
+              right={0}
+              display={{ base: "none", lg: "block" }}
+              bg={"brand.gold"}
+              boxSize={"100px"}
+              style={{ clipPath: "polygon(100% 0, 100% 100%,  0 100%)" }}
+            ></Box>
+            <Heading w={{ base: "80%", lg: "500px" }}>
+              Start your application to become a software engineer
+            </Heading>
+            <Text w={{ base: "80%", lg: "600px" }}>
+              Become a student at Bloom Institute of Technology. The next
+              application deadline is: December 10, 2021
+            </Text>
+            <Button
+              variant={"solid"}
+              bg={"brand.gold"}
+              color={"brand.deepgrey"}
+              w="150px"
+              size={"lg"}
+            >
+              <ReactRouterLink to="/about">Learn more</ReactRouterLink>
+            </Button>
           </Stack>
-          <Stack></Stack>
-        </Flex>
+        </Stack>
       </Box>
       <Footer />
     </Box>
